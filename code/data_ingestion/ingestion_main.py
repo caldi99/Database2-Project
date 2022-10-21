8aa5177ff865579e3ea187a141f4f4c8cf46556a
Andrea Pasin <andrea.pasin99@gmail.com>
2022-10-21 13:15:07 +0200

    Added main ingestion file with xml parser function

---
+++ code/data_ingestion/ingestion_main.py
@@ -0,0 +1,26 @@
+from xml.etree import ElementTree
+import sys
+
+
+# Parses the xml configuration file providing a dictionary with the paths to the datasets
+def get_dataset_paths(config_path):
+    tree = ElementTree.parse(config_path)
+    root = tree.getroot()
+    paths={}
+    for child in root.iter('path'):
+        paths[child.attrib['name']]=child.attrib['value']
+    return paths
+
+# Get the first arg passed through CLI which is the path_config.xml path
+# 'paths' is a dictionary with couples (key,path)
+paths={}
+try:
+    paths=get_dataset_paths(sys.argv[0])
+except:
+    print("Please execute this script by passing the path to the 'path_config.xml' file as argument")
+    exit()
+
+#
+# Invoke here the python files to serialize ttl files
+#
+
