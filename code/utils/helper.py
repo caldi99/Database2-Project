096a24be46a4fd55eacea361a6f9d5ac7b426439
Francesco <francesco.caldivezzi@hotmail.it>
2022-10-22 12:37:13 +0200

    fix

---
+++ code/utils/helper.py
@@ -0,0 +1,61 @@
+"""
+    Author : Francesco Caldivezzi
+"""
+
+import pandas as pd
+from rdflib import Graph
+from xml.etree import ElementTree
+
+class Helper:
+    """
+        This is a helper class for doing all the dirty jobs.
+    """
+
+    SERIALIZATION_TYPE = "turtle"    
+    CODEC = "utf-8"
+    PATHS_XML_PATH = "../config/paths.xml"
+    
+    PATH_NODE_XML = "path"
+    NAME_ATTRIBUTE_XML = "name"
+    VALUE_ATTRIBUTE_XML = "value"
+
+
+    def serialize(self,graph: Graph, output_path: str) -> None :
+        """
+            Serialize graph in output_path
+            Parameters 
+            graph : rdflib.Graph
+                        The graph to serialize
+            output_path : str
+                        The output path where to serialize the graph
+        """
+        with open(output_path) as file:
+            file.write(graph.serialize(format = self.SERIALIZATION_TYPE).decode(self.CODEC))        
+
+    def read_csv(self, path_csv: str) -> pd.DataFrame:
+        """
+            Read the csv file
+            Paramters :
+                path_csv : str
+                    The path where the csv is located
+            Returns :
+                The dataframe with the elements of the csv inside
+        """
+        return pd.read_csv(path_csv)
+
+    
+    def get_csv_path(self, name_file: str) -> str:
+        """
+            Returns the path of the name_file 
+            Parameters :
+                name_file : str 
+                    The name of the .csv file
+            Returns
+                Path of the .csv file
+        """
+        tree = ElementTree.parse(self.PATHS_XML_PATH)
+        root = tree.getroot()        
+        for child in root.iter(self.PATH_NODE_XML):
+            if(child.attrib[self.NAME_ATTRIBUTE_XML] == name_file):
+                return child.attrib[self.VALUE_ATTRIBUTE_XML]
+        return ""        
